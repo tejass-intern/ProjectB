0812fca6995c9474c890a7de5b44db89191789cf
Tejass Sanker J <tejass.intern@phonepe.com>
2022-04-06 14:38:09 +0530

    Add readme file

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+git init
+git pull https://github.com/tejass-intern/ProjectA.git
+
+git add .
+
+git commit -m "Add readme file"
+
+git push https://github.com/tejass-intern/ProjectB.git
